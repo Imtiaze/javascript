a5e14497fdae27e5c1882909150ec0ed9e61a02d
DESKTOP-JPKH2QN\Admin <ahammedimtiaze78@gmail.com>
2022-04-01 10:38:54 +0600

    JSX and js expression

--- 60 days of react/first-app/src/index.js
+++ 60 days of react/first-app/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';;
+import ReactDOM from 'react-dom';
+
+const title = 'Call Fami ly'
+const description = 'loremip sum text goes here'
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth();
+const year = date.getFullYear();
 
 ReactDOM.render(
   <div>
     <h1>Todo app</h1>
-    <p>You can enlist you daily works</p>
-    <a href='google.com'>Here you go</a>
-    <h4>31 March 2022</h4>
+    <h3>{title}</h3>
+    <p>{ description }</p>
+    <h4>{ day +'/'+ month +'/'+ year }</h4>
   </div>,
   document.getElementById('root')
 );

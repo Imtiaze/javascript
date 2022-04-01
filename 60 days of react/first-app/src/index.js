7a1c958154008a73a91a990cef49707fb3acfd08
DESKTOP-JPKH2QN\Admin <ahammedimtiaze78@gmail.com>
2022-04-01 08:46:54 +0600

    React do not support multi tag, only a single element

--- 60 days of react/first-app/src/index.js
+++ 60 days of react/first-app/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';;
 
 ReactDOM.render(
-  <h1>Hello React !</h1>,
+  <div>
+    <h1>Todo app</h1>
+    <p>You can enlist you daily works</p>
+    <a href='google.com'>Here you go</a>
+    <h4>31 March 2022</h4>
+  </div>,
   document.getElementById('root')
 );
